0a89bee2795844a4d823ec364bd464b5dbe08871
Stiffer Doroskevich <stiffer17@gmail.com>
2020-08-20 19:22:46 -0400

    ContactList new Class: Extending built-ins

--- poo.py
+++ poo.py
@@ -18,8 +18,22 @@ class Point():
             (self.x - other_point.x)**2 + (self.y - other_point.y)**2 # pitagoras
             )
     
+
+'''Instead of instantiating a normal list as our class variable, 
+we create a new ContactList class that extends the built-in list data type. 
+Then, we instantiate this subclass as our all_contacts list. '''
+
+class ContactList(list):
+    def search(self, name):
+        "Return all contacts that contain the search value in their name"
+        matching_contacts = []
+        for contact in self:
+            if name in contact.name:
+                matching_contacts.append(contact)
+        return matching_contacts
+
 class Contact: # basic inheritance
-    all_contacts = []
+    all_contacts = ContactList()
 
     def __init__(self, name, email):
         self.name = name
@@ -29,6 +43,6 @@ class Contact: # basic inheritance
 '''What if some contacs are also suppliers that we need to order supplies from?
    We could add an *order*, but that would allow people to accidentally order things from contacts'''
 
-class Suppliers(Contact):
+class Suppliers(Contact): # Extend class Contact
     def order(self, order):
         print("If this were a real system we would send order {0} to {1}, order {0} again".format(order, self.name))
